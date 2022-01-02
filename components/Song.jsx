95ff28c9a708915e51053293e109c42ae4331062
leolikestocode <leonardofpereira25@gmail.com>
2022-01-02 10:49:51 -0300

    Update deployment action, add env vars

--- components/Song.jsx
+++ components/Song.jsx
@@ -19,7 +19,7 @@ function Song({ track, order }) {
   };
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div className="grid grid-cols-2 text-gray-500 py-2 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
       <div className="flex items-center space-x-4" onClick={playSong}>
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={track.album.images[0].url} alt="" />
